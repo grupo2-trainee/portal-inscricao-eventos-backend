dfc271ab93eb79258799db8dbf0799f70e027521
Silvestre <silvestrelago@protonmail.com>
2025-05-25 11:10:55 -0300

    feat/Finalizando as configurações iniciais.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+import { PrismaClient } from '../generated/prisma/index.js'
+const prisma = new PrismaClient()
+
 const home = (req, res) =>{
     const { msg } = req.query
     res.status(200).json({

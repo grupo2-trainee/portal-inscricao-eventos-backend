42ffa82b4feb18837cb24513410c0f2b9d364b30
Silvestre <silvestrelago@protonmail.com>
2025-05-28 18:49:27 -0300

    fix: Corrigindo alguns erros de lógica em remove e edit admin.

--- controllers/authEvent.js
+++ controllers/authEvent.js
@@ -3,23 +3,24 @@ import jwt from 'jsonwebtoken'
 const JWT_SECRET = process.env.JWT_SECRET || 'includeJr'
 const prisma = new PrismaClient()
 
+// CADASTRAR EVENTO
 const cadEvent = async(req,res)=>{
     const { nome, descricao, dataInicio, dataFim, categoria, cidade, refreshToken} = req.body
 
     if(nome.length < 4){
-        res.status(400).json({erro: 'Nome do evento deve ter no mínimo 4 caracteres.'})
+        return res.status(400).json({erro: 'Nome do evento deve ter no mínimo 4 caracteres.'})
     }
 
     if(dataInicio > dataFim){
-        res.status(400).json({erro: 'Data de início deve ser anterior ou na mesma data da data de fim.'})
+        return res.status(400).json({erro: 'Data de início deve ser anterior ou na mesma data da data de fim.'})
     }
 
     if(!cidade || !categoria || !dataInicio || !dataFim || !descricao){
-        res.status(400).json({erro: 'Todos os campos são obrigatórios '})
+        return res.status(400).json({erro: 'Todos os campos são obrigatórios '})
     }
 
     if(!refreshToken){
-        res.status(403).json({erro: 'Usuário não autenticado.'})
+        return res.status(403).json({erro: 'Usuário não autenticado.'})
     }
 
     let idAdmin
@@ -28,7 +29,7 @@ const cadEvent = async(req,res)=>{
         idAdmin = decodedToken.id
 
     } catch (err) {
-        res.status(403).json({ erro: 'Token inválido ou expirado.' })
+        return res.status(403).json({ erro: 'Token inválido ou expirado.' })
     }
 
     const quantidadeInscritos = 0
@@ -49,22 +50,36 @@ const cadEvent = async(req,res)=>{
             }
         })
 
-        res.status(200).json({sucesso: "Evento criado com sucesso.", evento: novoEvento})
+        return res.status(200).json({sucesso: "Evento criado com sucesso.", evento: novoEvento})
 
     }catch(erro){
-        res.status(500).json({erro: 'Erro ao criar evento.', detalhes: erro})
+        return res.status(500).json({erro: 'Erro ao criar evento.', detalhes: erro})
     }
 }
 
+// REMOVER EVENTO
 const remEvent = async(req, res)=>{
-    const {nome} = req.body
+    const {id, refreshToken} = req.body
 
-    if(!nome){
-        res.status(400).json({erro: 'É necessario preencher o campo'})
+    if(!id, !refreshToken){
+        return res.status(400).json({erro: 'Dados inválidos.'})
     }
+
+    let idAdmin
+    try {
+        const decodedToken = jwt.verify(refreshToken, JWT_SECRET)
+        idAdmin = decodedToken.id
+
+    } catch (err) {
+        return res.status(403).json({ erro: 'Token inválido ou expirado.' })
+    }
+
     try {
         const evento = await prisma.evento.findUnique({
-            where: { nome }
+            where: { 
+                id,
+                idAdmin
+             }
         })
 
         if (!evento) {
@@ -72,46 +87,67 @@ const remEvent = async(req, res)=>{
         }
 
         await prisma.evento.delete({
-            where: { nome }
+            where: { id, idAdmin}
         })
 
-        res.status(200).json({ sucesso: 'Evento removido com sucesso.' })
+        return res.status(200).json({ sucesso: 'Evento removido com sucesso.' })
     } catch (erro) {
-        res.status(500).json({ erro: 'Erro ao remover evento.', detalhes: erro })
+        return res.status(500).json({ erro: 'Erro ao remover evento.', detalhes: erro })
     }
     
 }
 
+// EDITAR EVENTO
 const ediEvent = async (req, res) => {
-  const { nome, descricao, dataInicio, dataFim, categoria, cidade } = req.body
+    const { id, nome, descricao, dataInicio, dataFim, categoria, cidade, refreshToken } = req.body
 
-  if (!nome) {
-    res.status(400).json({ erro: 'É necessário preencher o campo nome.' })
-  }
-
-  try {
-    const evento = await prisma.evento.findUnique({ where: { nome } })
-    if (!evento) {
-      res.status(404).json({ erro: 'Evento não encontrado.' })
+    if(!id) {
+        return res.status(400).json({ erro: 'Dados inválidos.' })
     }
 
-    const eventoAtualizado = await prisma.evento.update({
-      where: { nome },
-      data: {
-        descricao,
-        dataInicio: new Date(dataInicio),
-        dataFim: new Date(dataFim),
-        categoria,
-        cidade
-      }
-    })
+    if(nome.length < 4){
+        return res.status(400).json({erro: 'Nome do evento deve ter no mínimo 4 caracteres.'})
+    }
 
-    res.status(200).json({ sucesso: 'Evento editado com sucesso.', evento: eventoAtualizado })
-  } catch (erro) {
-    res.status(500).json({ erro: 'Erro ao editar evento.', detalhes: erro.message })
-  }
+    if(dataInicio > dataFim){
+        return res.status(400).json({erro: 'Data de início deve ser anterior ou na mesma data da data de fim.'})
+    }
+
+    let idAdmin
+    try {
+        const decodedToken = jwt.verify(refreshToken, JWT_SECRET)
+        idAdmin = decodedToken.id
+
+    } catch (err) {
+        return res.status(403).json({ erro: 'Token inválido ou expirado.' })
+    }
+
+    const dataEdit = {}
+    if(nome != null) dataEdit.nome = nome
+    if(descricao != null) dataEdit.descricao = descricao
+    if(dataInicio != null) dataEdit.dataInicio = dataInicio
+    if(dataFim != null) dataEdit.dataFim = dataFim
+    if(categoria != null) dataEdit.categoria = categoria
+    if(cidade != null) dataEdit.cidade = cidade
+
+    try {
+        const evento = await prisma.evento.findUnique({ where: { id, idAdmin } })
+        if (!evento) {
+            return res.status(404).json({ erro: 'Evento não encontrado.' })
+        }
+
+        const eventoAtualizado = await prisma.evento.update({
+            where: { id, idAdmin },
+            data: dataEdit
+        })
+
+        return res.status(200).json({ sucesso: 'Evento editado com sucesso.'})
+    } catch (erro) {
+        return res.status(500).json({ erro: 'Erro ao editar evento.', detalhes: erro.message })
+    }
 }
 
+// EXPORTAÇÕES
 export default {
     ediEvent,
     remEvent,
